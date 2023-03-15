4a8cda07022cf6dfbd9e21ba6c066a14dcaccaf9
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-15 10:03:37 -0700

    part 1 and 2

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function printOdds(count) {
+    if (count < 1){
+        console.log('Please enter a positive number.');
+        return
+    }
+}
+
+
+for (let i = 1; i <= 50; i++){
+    if (i % 2 !==0){
+        console.log(i);
+    }
+}
+
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(userName, age){
+
+
+    let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`
+    let aboveSixteen = `Congrats ${userName}, you can drive!`
+
+if (age > 16) {
+    console.log(aboveSixteen);
+} 
+else {
+    console.log(belowSixteen);
+}
+}
+checkAge("AJ",32)
+checkAge("Ladd",14)
+
+
+// ex 3
+
+function checkPoint (x,y){
+   if  (x === 0 && y === 0){
+    console.log(`this is the origin`)
+   }
+   else if (x===0){
+    console.log(`(${x},)`)
+   }
+}
